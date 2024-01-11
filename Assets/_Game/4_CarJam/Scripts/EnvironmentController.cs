112a6c3dfd9e2f7498bc7c47662cee08b0347f08
ali kesgin <kesginali95@gmail.com>
2024-01-11 15:29:12 -0500

    [Added] Confetti And Car Centering

--- Assets/_Game/4_CarJam/Scripts/EnvironmentController.cs
+++ Assets/_Game/4_CarJam/Scripts/EnvironmentController.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace _Game._4_CarJam.Scripts
@@ -8,7 +9,8 @@ namespace _Game._4_CarJam.Scripts
         [SerializeField] private Transform _bottomTransform;
         [SerializeField] private Transform _leftTransform;
         [SerializeField] private Transform _rightTransform;
-
+        [SerializeField] private ParticleSystem _leftParticleSystem;
+        [SerializeField] private ParticleSystem _rightParticleSystem;
 
         public void Initialize(GridController gridController)
         {
@@ -17,6 +19,13 @@ namespace _Game._4_CarJam.Scripts
             _bottomTransform.localPosition = new Vector3(0, 0, rect.yMin);
             _leftTransform.localPosition = new Vector3(rect.xMin, 0, 0);
             _rightTransform.localPosition = new Vector3(rect.xMax + 1, 0, rect.yMax - 1);
+        }
+
+        [Button]
+        public void PlayParticles()
+        {
+            _leftParticleSystem.Play();
+            _rightParticleSystem.Play();
         }
     }
 }
